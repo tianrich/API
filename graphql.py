2e549cdf5000ca9993ed0db85728f659603b9a39
Tian R <78232090+tianrich@users.noreply.github.com>
2023-08-29 16:57:38 +0100

    Update graphql.py

--- graphql.py
+++ graphql.py
@@ -1,39 +1,50 @@
 import requests
 import json
+import argparse
 
-def get_api_info(api_url):
-    response = requests.get(api_url)
+def get_endpoints(url):
+  """Gets all endpoints from the API."""
+  response = requests.get(url)
+  if response.status_code != 200:
+    raise Exception("Failed to get endpoints: {}".format(response.status_code))
+
+  data = json.loads(response.content)
+  endpoints = []
+  for endpoint in data["paths"]:
+    endpoints.append(endpoint["path"])
+
+  return endpoints
+
+def get_security_features(url, endpoints):
+  """Gets the security features for each endpoint."""
+  security_features = {}
+  for endpoint in endpoints:
+    response = requests.get(url + endpoint)
     if response.status_code != 200:
-        raise ValueError("API call failed")
-
-    return json.loads(response.content)
-
-def main():
-    api_url = "https://your-graphql-api-url.com/"
-    api_info = get_api_info(api_url)
-
-    # Check for introspection vulnerabilities
-    introspection_query = """
-    query IntrospectionQuery {
-        __schema {
-            types {
-                name
-            }
-        }
-    }
-    """
-
-    response = requests.post(api_url, json={"query": introspection_query})
-    if response.status_code != 200:
-        raise ValueError("API call failed")
+      continue
 
     data = json.loads(response.content)
+    security_features[endpoint] = data["security"]
 
-    # Check for known vulnerabilities in the introspection response
-    for type in data["data"]["__schema"]["types"]:
-        # Check for known vulnerabilities in this type
-        if type["name"] in KNOWN_VULNERABILITIES:
-            print("Vulnerability found in type:", type["name"])
+  return security_features
+
+def save_to_file(security_features, filename):
+  """Saves the security features to a file."""
+  with open(filename, "w") as f:
+    json.dump(security_features, f)
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--url", help="The URL of the API")
+  parser.add_argument("--filename", help="The name of the file to save the security features to")
+  args = parser.parse_args()
+
+  url = args.url
+  filename = args.filename
+
+  endpoints = get_endpoints(url)
+  security_features = get_security_features(url, endpoints)
+  save_to_file(security_features, filename)
 
 if __name__ == "__main__":
-    main()
+  main()
